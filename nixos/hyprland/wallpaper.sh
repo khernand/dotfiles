8b3f18621aeaa5cbd55bbbdc26680249a65935ba
Kevin Hernandez <2459865+khernand@users.noreply.github.com>
2025-02-02 15:08:19 -0500

    chore(dotfiles): Auto-update 2025-02-02

---
+++ nixos/hyprland/wallpaper.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+WALLPAPER_DIR="$HOME/wallpapers/walls"
+#I dont know what the fuck I am doing
+menu() {
+    find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) | awk '{print "img:"$0}'
+}
+main() {
+    choice=$(menu | wofi -c ~/.config/wofi/config1 -s ~/.config/wofi/style1.css --show dmenu --prompt "Select Wallpaper:" -n)
+    selected_wallpaper=$(echo "$choice" | sed 's/^img://')
+    swww img "$selected_wallpaper" --transition-type any --transition-fps 60 --transition-duration 1.5
+    wal -i "$selected_wallpaper" -n --cols16
+    swaync-client --reload-css
+    cat ~/.cache/wal/colors-kitty.conf > ~/.config/kitty/current-theme.conf
+    pywalfox update
+    color1=$(awk 'match($0, /color2=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
+    color2=$(awk 'match($0, /color3=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh)
+    cava_config="$HOME/.config/cava/config"
+    sed -i "s/^gradient_color_1 = .*/gradient_color_1 = '$color1'/" $cava_config
+    sed -i "s/^gradient_color_2 = .*/gradient_color_2 = '$color2'/" $cava_config
+    pkill -USR2 cava || cava -p $cava_config
+    source ~/.cache/wal/colors.sh && cp -r $wallpaper ~/wallpapers/pywallpaper.jpg 
+}
+main
